8a31868712b9f253fe69ea3c4853098d7c8e5d76
Dustin Carrion <dustin.carrion@gmail.com>
2023-02-06 15:41:03 +0100

    NCC added to fine-tuning baseline

--- baselines/finetuning/helpers_finetuning.py
+++ baselines/finetuning/helpers_finetuning.py
@@ -4,10 +4,10 @@ import torch.nn as nn
 from typing import Tuple
 
 
-def optimize(model: nn.Module, 
-             optimizer: torch.optim, 
-             X: torch.Tensor, 
-             y: torch.Tensor) -> Tuple[torch.Tensor, float]:
+def optimize_linear(model: nn.Module, 
+                    optimizer: torch.optim, 
+                    X: torch.Tensor, 
+                    y: torch.Tensor) -> Tuple[torch.Tensor, float]:
     """ Apply an optimization step using the specified model, optimizer and 
     data.
 
@@ -25,7 +25,7 @@ def optimize(model: nn.Module,
     loss = model.criterion(out, y)
     loss.backward()
     optimizer.step()
-    return out, loss.item()
+    return out, loss
 
 
 def get_batch(X: torch.Tensor, 
@@ -44,3 +44,56 @@ def get_batch(X: torch.Tensor,
     batch_indices = np.random.randint(0, X.size()[0], batch_size)
     X_batch, y_batch = X[batch_indices], y[batch_indices]
     return X_batch, y_batch
+
+
+def process_support_set(model: nn.Module, 
+                        X_train: torch.Tensor, 
+                        y_train: torch.Tensor, 
+                        num_classes: int) -> torch.Tensor:
+    """ Process the support set following the NCC strategy.
+
+    Args:
+        model (nn.Module): Model to be used.
+        X_train (torch.Tensor): Support set images.
+        y_train (torch.Tensor): Support set labels.
+        num_classes (int): Number of classes to predict.
+
+    Returns:
+        torch.Tensor: Support prototypes.
+    """
+    # Compute input embeddings
+    support_embeddings = model(X_train, embedding=True)
+    
+    # Compute prototypes
+    prototypes = torch.zeros((num_classes, support_embeddings.size(1)), 
+        device=X_train.device)
+    for i in range(num_classes):
+        mask = y_train == i
+        prototypes[i] = (support_embeddings[mask].sum(dim=0) / 
+            torch.sum(mask).item())
+        
+    return prototypes
+
+
+def process_query_set(model: nn.Module, 
+                      X_test: torch.Tensor, 
+                      prototypes: torch.Tensor) -> torch.Tensor:
+    """ Process the query set following the Matching Networks strategy.
+
+    Args:
+        model (nn.Module): Model to be used.
+        X_test (torch.Tensor): Query set images.
+        prototypes (torch.Tensor): Support prototypes.
+
+    Returns:
+        torch.Tensor: Distances to prototypes.
+    """
+    # Compute input embeddings
+    query_embeddings = model(X_test, embedding=True)
+
+    # Create distance matrix (negative predictions)
+    distance_matrix = (torch.cdist(query_embeddings.unsqueeze(0), 
+        prototypes.unsqueeze(0))**2).squeeze(0) 
+    out = -1 * distance_matrix
+    
+    return out
--- baselines/finetuning/model.py
+++ baselines/finetuning/model.py
@@ -7,6 +7,7 @@ network initilization at meta-training time can be with:
 import os
 import random
 import pickle
+import contextlib
 import numpy as np
 import torch
 import torch.nn as nn
@@ -72,6 +73,8 @@ class MyMetaLearner(MetaLearner):
         
         # General data parameters
         self.should_train = True
+        self.ncc = False
+        self.support_size = 12
         self.train_batches = 20
         self.val_tasks = 10
         self.val_after = 5
@@ -94,6 +97,9 @@ class MyMetaLearner(MetaLearner):
         self.meta_learner.train()
         self.optimizer = self.opt_fn(self.meta_learner.parameters(), 
             lr=self.lr, betas=(self.beta1, self.beta2))
+        
+        # Initialize running prototypes
+        if self.ncc: self.init_prototypes()
         
         # Validation-learner
         self.best_score = -float("inf")
@@ -132,12 +138,34 @@ class MyMetaLearner(MetaLearner):
                 X_train = X_train.to(self.dev)
                 y_train = y_train.view(-1).to(self.dev)
                 
-                # Optimize metalearner
-                out, loss = optimize(self.meta_learner, self.optimizer, 
-                    X_train, y_train)
+                if self.ncc:
+                    # Create prototypes for each class
+                    if 0 in self.running_lenght:
+                        self.update_prototypes(X_train, y_train)
+
+                    # Optimize metalearner
+                    else:                        
+                        # Prepare data as a task
+                        (X_train, y_train, 
+                        X_test, y_test) = self.batch_to_task(X_train, y_train, 
+                            self.support_size)
+
+                        self.optimizer.zero_grad()
+                        out, loss = self.optimize_ncc(X_train, y_train, X_test, 
+                            y_test, True, None)
+                        loss.backward()
+                        self.optimizer.step()  
+                        
+                        # Log iteration
+                        self.log((X_test, y_test), out.detach().cpu().numpy(), 
+                            loss.item())
+                else:
+                    # Optimize metalearner
+                    out, loss = optimize_linear(self.meta_learner, 
+                        self.optimizer, X_train, y_train)
                 
-                # Log iteration
-                self.log(batch, out.detach().cpu().numpy(), loss)
+                    # Log iteration
+                    self.log(batch, out.detach().cpu().numpy(), loss.item())
                 
                 if (i + 1) % self.val_after == 0:
                     self.meta_valid(meta_valid_generator)
@@ -151,6 +179,7 @@ class MyMetaLearner(MetaLearner):
             "lr": self.val_lr,
             "T": self.T,
             "batch_size": self.val_batch_size,
+            "ncc": self.ncc
         }
         return MyLearner(self.model_args, self.best_state, learner_params)
     
@@ -179,16 +208,23 @@ class MyMetaLearner(MetaLearner):
             val_optimizer = self.opt_fn(self.val_learner.parameters(), 
                 self.val_lr)
             
-            # Optimize learner
-            for _ in range(self.T):        
-                X_batch, y_batch = get_batch(X_train, y_train, 
-                    self.val_batch_size)
-                optimize(self.val_learner, val_optimizer, X_batch, y_batch)
+            if self.ncc:
+                # Evaluate learner
+                out, _ = self.optimize_ncc(X_train, y_train, X_test, y_test, 
+                    False, num_ways)
+            else:
+                # Optimize learner
+                for _ in range(self.T):        
+                    X_batch, y_batch = get_batch(X_train, y_train, 
+                        self.val_batch_size)
+                    optimize_linear(self.val_learner, val_optimizer, X_batch, 
+                        y_batch)
 
-            # Evaluate learner
-            with torch.no_grad():
-                out = self.val_learner(X_test)
-                preds = torch.argmax(out, dim=1).cpu().numpy()
+                # Evaluate learner
+                with torch.no_grad():
+                    out = self.val_learner(X_test)
+            
+            preds = torch.argmax(out, dim=1).cpu().numpy()
             
             # Log iteration
             self.log(task, out.cpu().numpy(), meta_train=False)
@@ -204,6 +240,16 @@ class MyMetaLearner(MetaLearner):
             self.best_state = {k : v.clone() for k, v in 
                 self.meta_learner.state_dict().items()}
 
+    def init_prototypes(self) -> None:
+        """ Initialize the prototypes for the NCC classifier with batch 
+        learning.
+        """
+        self.running_prototypes = torch.zeros((self.train_classes, 
+            self.meta_learner.in_features), device=self.get_device(), 
+            requires_grad=False)
+        self.running_lenght = torch.zeros(self.train_classes, 
+            device=self.get_device(), requires_grad=False)
+
     def get_device(self) -> torch.device:
         """ Get the current device, it can be CPU or GPU.
 
@@ -218,6 +264,125 @@ class MyMetaLearner(MetaLearner):
             print("Using CPU")
         return device
     
+    def update_prototypes(self, 
+                          X: torch.Tensor, 
+                          y: torch.Tensor,
+                          grad: bool = False) -> Tuple[torch.Tensor, 
+                                                       torch.Tensor]:
+        """ Update the prototypes following the NCC strategy.
+
+        Args:
+            X (torch.Tensor): Images.
+            y (torch.Tensor): Labels.
+            grad (bool, optional): Boolean flag to indicate if the grad should
+                be tracked.  
+                
+        Returns:
+            Tuple[torch.Tensor, torch.Tensor]: Running prototypes and lenght 
+                with gradient. It is only returned if the gradient is required.
+        """
+        if grad:
+            context = self.empty_context
+        else:
+            context = torch.no_grad
+
+        # Compute embeddings
+        with context():
+            current_embeddings = self.meta_learner(X, embedding=True)
+            if grad:
+                no_grad_embeddings = current_embeddings.clone().detach()
+                grad_running_prototypes = self.running_prototypes.clone()
+                grad_running_lenght = self.running_lenght.clone()
+            else:
+                no_grad_embeddings = current_embeddings
+        
+            # Update prototypes
+            for i in range(self.train_classes):
+                mask = y == i
+                self.running_prototypes[i] += no_grad_embeddings[mask].sum(dim=0) 
+                self.running_lenght[i] += torch.sum(mask).item()
+                
+                if grad:
+                    grad_running_prototypes[i] += current_embeddings[mask].sum(dim=0) 
+                    grad_running_lenght[i] += torch.sum(mask).item()
+                    
+        if grad:
+            return grad_running_prototypes, grad_running_lenght
+                
+    
+    def batch_to_task(self,
+                      X: torch.Tensor, 
+                      y: torch.Tensor, 
+                      support_size: int) -> Tuple[torch.Tensor, torch.Tensor, 
+                                              torch.Tensor, torch.Tensor]:
+        """ Transform a batch into a task (support and query sets).
+
+        Args:
+            X (torch.Tensor): Images.
+            y (torch.Tensor): Labels.
+            support_size (int): Desired support size.
+
+        Returns:
+            Tuple[torch.Tensor, torch.Tensor, torch.Tensor, torch.Tensor]: 
+                Support images, support labels, query images, and query labels.
+        """
+        # Select support and query indices
+        all_indices = list(range(0, X.shape[0]))
+        support_indices = np.random.choice(all_indices, size=support_size, 
+            replace=False)
+        query_indices = [i for i in all_indices if i not in support_indices]
+
+        # Split batch into support and query sets
+        X_train = X[support_indices, :, :, :]
+        y_train = y[support_indices]
+        X_test = X[query_indices, :, :, :]
+        y_test = y[query_indices]
+
+        return X_train, y_train, X_test, y_test
+    
+    def optimize_ncc(self,
+                     X_train: torch.Tensor, 
+                     y_train: torch.Tensor, 
+                     X_test: torch.Tensor, 
+                     y_test: torch.Tensor,
+                     training: bool, 
+                     num_classes: int) -> Tuple[torch.Tensor, torch.Tensor]:
+        """ Compute the output and loss using the specified data.
+
+        Args:
+            X_train (torch.Tensor): Support set images.
+            y_train (torch.Tensor): Support set labels.
+            X_test (torch.Tensor): Query set images.
+            y_test (torch.Tensor): Query set labels.
+            training (bool): Boolean flag to control the execution context. If 
+                True, keep track of the gradients, otherwise the gradients are 
+                ignored.
+            num_classes (int): Number of classes to predict.
+
+        Returns:
+            Tuple[torch.Tensor, torch.Tensor]: Output and loss.
+        """
+        if training:
+            run_prototypes, run_lenght = self.update_prototypes(X_train, 
+                y_train, grad=True)
+            prototypes = run_prototypes / run_lenght.unsqueeze(1)
+            out = process_query_set(self.meta_learner, X_test, prototypes)
+            loss = self.meta_learner.criterion(out, y_test) 
+        else:
+            with torch.no_grad():
+                prototypes = process_support_set(self.val_learner, X_train, 
+                    y_train, num_classes)
+                out = process_query_set(self.val_learner, X_test, prototypes)
+                loss = None
+            
+        return out, loss
+
+    @contextlib.contextmanager
+    def empty_context(self) -> None:
+        """ Defines an empty context to avoid computing unnecessary gradients.
+        """
+        yield None
+
 
 class MyLearner(Learner):
 
@@ -264,14 +429,20 @@ class MyLearner(Learner):
         X_train, y_train = X_train.to(self.dev), y_train.to(self.dev)
         
         self.learner.freeze_layers(n_ways)
-        optimizer = self.opt_fn(self.learner.parameters(), self.lr)
         
-        # Optimize learner
-        for i in range(self.T):        
-            X_batch, y_batch = get_batch(X_train, y_train, self.batch_size)
-            optimize(self.learner, optimizer, X_batch, y_batch)
+        if self.ncc:
+            with torch.no_grad():
+                prototypes = process_support_set(self.learner, X_train, 
+                    y_train, n_ways)
+        else:
+            # Optimize learner
+            optimizer = self.opt_fn(self.learner.parameters(), self.lr)
+            for _ in range(self.T):        
+                X_batch, y_batch = get_batch(X_train, y_train, self.batch_size)
+                optimize_linear(self.learner, optimizer, X_batch, y_batch)
+            prototypes = None
         
-        return MyPredictor(self.learner, self.dev)
+        return MyPredictor(self.learner, self.dev, prototypes)
 
     def save(self, path_to_save: str) -> None:
         """ Saves the learning object associated to the Learner. 
@@ -328,6 +499,7 @@ class MyLearner(Learner):
             self.lr = self.learner_params["lr"]
             self.T = self.learner_params["T"]
             self.batch_size = self.learner_params["batch_size"]
+            self.ncc = self.learner_params["ncc"]
         else:
             raise Exception(f"'{learner_params_file}' not found")
         
@@ -336,16 +508,19 @@ class MyPredictor(Predictor):
 
     def __init__(self, 
                  model: nn.Module, 
-                 dev: torch.device) -> None:
+                 dev: torch.device,
+                 prototypes: torch.Tensor) -> None:
         """ Defines the Predictor initialization.
 
         Args:
             model (nn.Module): Fitted learner. 
             dev (torch.device): Device where the data is located.
+            prototypes (torch.device): Support prototypes.
         """
         super().__init__()
         self.model = model
         self.dev = dev
+        self.prototypes = prototypes
 
     def predict(self, query_set: torch.Tensor) -> np.ndarray:
         """ Given a query_set, predicts the probabilities associated to the 
@@ -367,7 +542,16 @@ class MyPredictor(Predictor):
         """
         X_test = query_set.to(self.dev)
         with torch.no_grad():
-            out = self.model(X_test)
+            if self.prototypes is not None:
+                # Compute input embeddings
+                query_embeddings = self.model(X_test, embedding=True)
+
+                # Create distance matrix (negative predictions)
+                distance_matrix = (torch.cdist(query_embeddings.unsqueeze(0), 
+                    self.prototypes.unsqueeze(0))**2).squeeze(0) 
+                out = -1 * distance_matrix
+            else:
+                out = self.model(X_test)
             probs = F.softmax(out, dim=1).cpu().numpy()
         
         return probs
